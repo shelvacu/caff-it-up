aa3b83c835ffb6276c7effe93248e9190b4a4ac1
Henry Troutman <henguin1001@gmail.com>
2014-11-08 13:51:28 -0800

    routes now go in the index.js file under the folder routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('test', { title: 'Express', body:"Henry is the coolest", seq:["henry", "is", "pretty", "great"] });
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('test', {
+			title: 'Express',
+			body: "Henry is the coolest",
+			seq: ["henry", "is", "pretty", "great"]
+		});
+	});
+
 };
