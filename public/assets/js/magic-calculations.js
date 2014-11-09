a8f9be90d7545c2fc2b0b99196999b8fe721d74f
Matthew0x40 <matthewhlee96@gmail.com>
2014-11-08 22:27:42 -0800

    Push

--- public/assets/js/magic-calculations.js
+++ public/assets/js/magic-calculations.js
@@ -4,12 +4,22 @@
 //
 //Output: Array of times and milligrams, eg
 //[[Date, 55],[Date, 55]]
-function inputToConsumption(params){
+function inputToConsumption(params){ 
     var start = new Date()
     var res = [[start, 40]] // 40 - One coke
+    
     var w = params.weightLbs;
     var Ao = params.drinkSizemg;
-    var t = ((280-w)/26)*(( Math.log(20/Ao) )/(-Math.log(2))); //next dose, in hours.
+    var A = 20;
+    
+    var t = Math.log( (A * (11.76 - 0.162))/(11.76 * Ao * (Math.pow(Math.E, -0.162) - Math.pow(Math.E, -11.76)) ) ); //next dose, in hours.
+    // var t = ((280-w)/26)*(( Math.log(20/Ao) )/(-Math.log(2))); Older equation
+    
+    if (t < 0) {
+        console.log('inputToConsumption error: t < 0; t=' + t);
+        return null;
+    }
+    
     var end = new Date();
     end.setTime(start.getTime());
     console.log(t);
